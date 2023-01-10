e4f40dcafd3cd5050c116a824995a55d0dd3207a
Adam Žalůdek <47R4sd@protonmail.com>
2023-01-10 21:28:56 +0100

    fix: webpage url

--- scraper.r
+++ scraper.r
@@ -1,1 +1,12 @@
-url <- read_html("https://skolabaltaci.cz/")
+library(rvest)
+
+url <- read_html("https://www.alexa.com/topsites/countries/IN")
+webpage <- read_html(url)
+
+header <- html_nodes(webpage, "h1")
+header_text <- html_text(header)
+
+urls <- html_nodes(webpage, "a") %>% html_attr("href")
+images <- html_nodes(webpage, "img") %>% html_attr("src")
+
+print(header_text)
